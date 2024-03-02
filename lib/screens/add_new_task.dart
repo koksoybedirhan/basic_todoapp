0258c9e3673ff2f9d66d08f276b33651e072c80b
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2024-03-03 01:38:48 +0300

    update03-03-24

--- lib/screens/add_new_task.dart
+++ lib/screens/add_new_task.dart
@@ -1,5 +1,8 @@
+import 'package:basic_todoapp/constants/color.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class AddNewTaskScreen extends StatelessWidget {
   const AddNewTaskScreen({super.key});
@@ -13,43 +16,158 @@ class AddNewTaskScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         //appBar: AppBar(), //Navigator'den gelen sayfaya geri donmek icin, otomatik doner
-        body: Column(
-          children: [
-            Container(
-              width: deviceWidth,
-              height: deviceHeight / 10,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image:
-                      AssetImage("lib/assets/images/add_new_task_header.png"),
-                  fit: BoxFit.cover,
+        backgroundColor: HexColor(backgroundColor),
+        body: SingleChildScrollView(
+          //ekran sabit kalmak yerine asagi yukari kayan ekran oldu
+          child: Column(
+            children: [
+              Container(
+                width: deviceWidth,
+                height: deviceHeight / 10,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image:
+                        AssetImage("lib/assets/images/add_new_task_header.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(
+                        Icons.close,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Expanded(
+                        child: Text(
+                      "Add new Task",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 21),
+                      textAlign: TextAlign.center,
+                    ))
+                  ],
                 ),
               ),
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    icon: const Icon(
-                      Icons.close,
-                      size: 40,
-                      color: Colors.white,
+              const Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: const Text("Task Title")),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: TextField(
+                  decoration:
+                      InputDecoration(filled: true, fillColor: Colors.white),
+                ),
+              ), //text icine yazi yazmayi aktif eder (telefon klavyesi ile)
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text("Category"),
+                    GestureDetector(
+                      //ekrana basmaya gore islem yapmak icin
+                      onTap: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            //butona basildiginda ne yapilmasi gerektiğini yapar
+                            duration: Duration(
+                                milliseconds:
+                                    300), //kac saniye calistigini belirten fonksiyon
+                            content: Text("Category Selected"),
+                          ),
+                        );
+                      },
+                      child: Image.asset("lib/assets/images/category_1.png"),
                     ),
-                  ),
-                  const Expanded(
-                      child: Text(
-                    "Add new Task",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 21),
-                    textAlign: TextAlign.center,
-                  ))
-                ],
+                    GestureDetector(
+                      //ekrana basmaya gore islem yapmak icin
+                      onTap: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            //butona basildiginda ne yapilmasi gerektiğini yapar
+                            duration: Duration(
+                                milliseconds:
+                                    300), //kac saniye calistigini belirten fonksiyon
+                            content: Text("Category Selected"),
+                          ),
+                        );
+                      },
+                      child: Image.asset("lib/assets/images/category_2.png"),
+                    ),
+                    GestureDetector(
+                      //ekrana basmaya gore islem yapmak icin
+                      onTap: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            //butona basildiginda ne yapilmasi gerektiğini yapar
+                            duration: Duration(
+                                milliseconds:
+                                    300), //kac saniye calistigini belirten fonksiyon
+                            content: Text("Category Selected"),
+                          ),
+                        );
+                      },
+                      child: Image.asset("lib/assets/images/category_3.png"),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+              const Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text("Date"),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  filled: true, fillColor: Colors.white),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text("Time"),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  filled: true, fillColor: Colors.white),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const Padding(
+                  padding: EdgeInsets.only(top: 10), child: Text("Notes")),
+              const SizedBox(
+                height: 280,
+                child: TextField(
+                  expands: true,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                      filled: true, fillColor: Colors.white, isDense: true),
+                ),
+              ),
+              ElevatedButton(onPressed: () {}, child: const Text("Save"))
+            ],
+          ),
         ),
       ),
     );
